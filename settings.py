f830e3c723cb6f65568ad872f15dbe88a0717cec
dennisg54 <dgellegani@student.cscc.edu>
2025-04-14 15:39:31 -0400

    Created a settings file and changed the BG to be an image

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+from pathlib import Path
+
+class Settings:
+    def __init__ (self) -> None:
+        self.name: str = "Alien Invasion"
+        self.screen_w: int = 1200
+        self.screen_h: int = 800
+        self.FPS = 60
+        self.bg_file = Path.cwd() / "Assets" / "images" / "Starbasesnow.png"
+        

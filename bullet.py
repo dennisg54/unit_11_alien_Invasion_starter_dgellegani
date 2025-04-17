834ef8f1271c1d113c753c0f616d5f2a7bac1ff4
dennisg54 <dgellegani@student.cscc.edu>
2025-04-17 15:41:56 -0400

    Corrected undefined variable "AlienInvasion" in bullet.py

--- bullet.py
+++ bullet.py
@@ -3,6 +3,9 @@ import pygame
 from pygame.sprite import Sprite
 from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
 class Bullet(Sprite):
     def __init__(self, game: "AlienInvasion") -> None:
         super().__init__()
